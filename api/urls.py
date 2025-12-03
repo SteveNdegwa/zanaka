692ec44325a9e3ed0425cb10c52b22ceabd02999
SteveNdegwa <stevencallistus19@gmail.com>
2025-12-03 11:16:46 +0300

    feat: finances app

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('auth/', include('authentication.urls')),
+    path('finances/', include('finances.urls')),
     path('otps/', include('otps.urls')),
     path('notifications/', include('notifications.urls')),
     path('schools/', include('schools.urls')),
