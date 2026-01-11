a8730cff2a6b4d1128221b07b35ac05553035a28
SteveNdegwa <stevencallistus19@gmail.com>
2026-01-11 04:00:51 +0300

    infra: integrate ci/cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,7 +157,7 @@ provisioner "remote-exec" {
           listen 80;
           server_name _;
 
-          location /flower/ {
+          location / {
               proxy_pass http://127.0.0.1:5555;
               proxy_set_header Host \$host;
               proxy_set_header X-Real-IP \$remote_addr;
