9670e8caaa242d14aaf1500a7f93afb7938be6db
SteveNdegwa <stevencallistus19@gmail.com>
2026-01-30 23:37:41 +0300

    updates

--- zanaka/settings.py
+++ zanaka/settings.py
@@ -97,12 +97,12 @@ AUTH_USER_MODEL = "users.User"
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        'NAME': os.environ.get("POSTGRES_DB", "zanaka"),
-        'USER': os.environ.get("POSTGRES_USER", "zanaka"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "secret"),
-        'HOST': os.environ.get("POSTGRES_HOST", "127.0.0.1"),
-        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'zanaka_postgres',
+        'PORT': '5432',
     }
 }
 
